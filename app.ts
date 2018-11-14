166022d4913efd867d2e932a30b9ae33884c8159
Stefan Daniel <stefan.daniel99@gmx.at>
2018-11-14 22:49:09 +0100

    app.ts modified

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 window.onload = function () {
     let fNameCor: boolean = false;
     let lNameCor: boolean = false;
-    let emailCor: boolean = true;
+    let emailCor: boolean = false;
     let emailEmpty: boolean = true;
     let newsletterChecked: boolean = false;
     let mediaChannelSelectOther: boolean = false;
