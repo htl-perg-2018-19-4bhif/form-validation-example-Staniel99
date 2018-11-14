458748f98b038afbfe76f0d77895de42f0827c08
Stefan Daniel <stefan.daniel99@gmx.at>
2018-11-14 22:47:31 +0100

    initial commit

---
+++ app.ts
@@ -0,0 +1,9 @@
+window.onload = function () {
+    let fNameCor: boolean = false;
+    let lNameCor: boolean = false;
+    let emailCor: boolean = true;
+    let emailEmpty: boolean = true;
+    let newsletterChecked: boolean = false;
+    let mediaChannelSelectOther: boolean = false;
+
+}
---
+++ dist/app.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    var fNameCor = false;
+    var lNameCor = false;
+    var emailCor = true;
+    var emailEmpty = true;
+    var newsletterChecked = false;
+    var mediaChannelSelectOther = false;
+};
